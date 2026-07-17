e28a56a3e2eaad20b7ae25a0e873a470db4baa04
PREME <preme@MacBookAirPREME.local>
2026-07-18 04:59:56 +0700

    feat: เพิ่ม validation ในการเพิ่มและอัปเดต

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -3,9 +3,15 @@ const db = require("../config/db");
 const createTrade = async (req, res) => {
   try {
     const { symbol, entry_price, pattern } = req.body;
+
+    if(!symbol || !entry_price || entry_price <= 0 || !pattern){
+        return res.status(400).json({ message: 'โปรดระบุข้อมูลให้ครบถ้วน และราคาต้องมากกว่า 0'})
+    }
+
     const sql =
       "INSERT INTO watchlist (symbol, entry_price, pattern) VALUES (?, ?, ?)";
     const [result] = await db.query(sql, [symbol, entry_price, pattern]);
+
 
     return res.status(201).json({
       message: "เพิ่มแผนการเทรดสำเร็จ",
@@ -52,6 +58,11 @@ const updateTrade = async (req, res) => {
   try {
     const tradeId = req.params.id;
     const { symbol, entry_price, pattern } = req.body;
+
+    if(!symbol || !entry_price || entry_price <= 0 || !pattern){
+        return res.status(400).json({ message: 'โปรดระบุข้อมูลให้ครบถ้วน และราคาต้องมากกว่า 0'})
+    }
+    
     const sql =
       "UPDATE watchlist SET symbol = ?, entry_price = ?, pattern = ? WHERE id = ?";
     const [result] = await db.query(sql, [
